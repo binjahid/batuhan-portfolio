3747b903c34311de35c8193f29c28a5f9ead3e95
binjahid <muhammadjahidbinmutalib@gmail.com>
2022-11-28 07:12:00 +0600

    error solved

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import ContactForm from "../../sections/ContactForm";
-import ContactHero from "../../sections/ContactHero";
 
 const ContactPage = () => {
   return (
     <>
-      {/* <ContactHero /> */}
       <Head>
         <title>Contact &mdash; Batuhan</title>
       </Head>
