046882188f5e74a874941b005f017cee614bc6aa
binjahid <muhammadjahidbinmutalib@gmail.com>
2022-11-28 07:09:19 +0600

    eslint ignored

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ["media.graphcms.com", 'media.graphassets.com']
-  }
-}
+    domains: ["media.graphcms.com", "media.graphassets.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
